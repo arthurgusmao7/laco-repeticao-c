fc1add788f46aab839252e41cd1c8f9065dcbb62
JMR_996 <mateusjoseph@gmail.com>
2026-04-10 21:26:31 -0300

    feat: Atv2.cs
    Inserindo a Atv2

---
+++ Atv02.cs
@@ -0,0 +1,35 @@
+public static class Atv02
+{
+    public static void Executar()
+    {
+        double valorCompra;
+        double totalArrecadado = 0;
+        int quantidadeCompras = 0;
+        string continuar;
+
+        do
+        {
+            Console.Write("Digite o valor da compra: R$ ");
+            valorCompra = double.Parse(Console.ReadLine()!);
+
+            if (valorCompra > 0)
+            {
+                totalArrecadado = totalArrecadado + valorCompra;
+                quantidadeCompras = quantidadeCompras + 1;
+            }
+            else
+            {
+                Console.WriteLine("Valor inválido. Digite um valor maior que zero.");
+            }
+
+            Console.Write("Deseja registrar outra compra? (s/n): ");
+            continuar = Console.ReadLine()!;
+            Console.WriteLine();
+
+        } while (continuar == "s" || continuar == "S");
+
+        Console.WriteLine("Quantidade de compras registradas: " + quantidadeCompras);
+        Console.WriteLine("Total arrecadado: R$ " + totalArrecadado);
+    }
+}
+
